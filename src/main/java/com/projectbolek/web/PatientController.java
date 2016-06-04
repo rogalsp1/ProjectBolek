4dbf9853c130293ddcded9494aa2c02c49548ab4
Marcin Prokop <marcin@prokop.co>
2016-06-05 01:44:41 +0200

    Change paths to proper REST paths in patient controller

--- src/main/java/com/projectbolek/web/PatientController.java
+++ src/main/java/com/projectbolek/web/PatientController.java
@@ -19,9 +19,9 @@ import java.util.List;
  * Created by rogalsp1 on 30.05.16.
  */
 @RestController
-@RequestMapping(path = "/patients/")
+@RequestMapping("/patients")
 @Slf4j
-public class PatientController implements Serializable{
+public class PatientController implements Serializable {
 
     private static final long serialVersionUID = -3606314241856173634L;
 
@@ -35,43 +35,43 @@ public class PatientController implements Serializable{
     }
 
     @RequestMapping(path = "/active", method = RequestMethod.GET)
-    public List<PatientDTO> getActivePatients(){
+    public List<PatientDTO> getActivePatients() {
         List<PatientEntity> patientList = patientService.findActivePatients();
         return converter.fromEntity(patientList);
     }
 
-    @RequestMapping(path = "/new", method = RequestMethod.POST)
-    public ResponseEntity<?> addNewPatient(@RequestBody PatientDTO patientDTO){
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> addNewPatient(@RequestBody PatientDTO patientDTO) {
         PatientEntity patient = converter.fromDTO(patientDTO);
         try {
             patientService.addNewPatient(patient);
         } catch (ApplicationException e) {
-            return new ResponseEntity<Object>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Object>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<Object>(null, HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/deactivate", method = RequestMethod.POST)
-    public ResponseEntity<?> deactivatePatient(@RequestParam("id") Long patientId){
-        patientService.deactivatePatient(patientId);
-        return new ResponseEntity<Object>(null,HttpStatus.OK);
-    }
-
-    @RequestMapping(path = "/contactdetails", method = RequestMethod.GET)
-    public ContactDetailsEntity getContactDetails(@RequestParam("id") Long patientId){
-        return patientService.findContactDetailsByPatientId(patientId);
-    }
-
-    @RequestMapping(path = "/update", method = RequestMethod.POST)
-    public ResponseEntity<?> updatePatient(@RequestBody PatientDTO patientDTO){
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<?> updatePatient(@RequestBody PatientDTO patientDTO) {
         PatientEntity patient = converter.fromDTO(patientDTO);
         patientService.save(patient);
         return new ResponseEntity<Object>(null, HttpStatus.OK);
     }
 
-    @RequestMapping(path= "/saveContact", method = RequestMethod.POST)
-    public ResponseEntity<?> saveContactDetails(@RequestBody ContactDetailsEntity contactDetailsEntity, @RequestParam("patient_id") Long id){
-        patientService.saveContactDetails(contactDetailsEntity,id);
+    @RequestMapping(path = "/{patientId}/deactivate", method = RequestMethod.POST)
+    public ResponseEntity<?> deactivatePatient(@PathVariable Long patientId) {
+        patientService.deactivatePatient(patientId);
+        return new ResponseEntity<Object>(null, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{patientId}/contactdetails", method = RequestMethod.GET)
+    public ContactDetailsEntity getContactDetails(@PathVariable Long patientId) {
+        return patientService.findContactDetailsByPatientId(patientId);
+    }
+
+    @RequestMapping(path = "/{patientId}/contactdetails", method = RequestMethod.POST)
+    public ResponseEntity<?> saveContactDetails(@RequestBody ContactDetailsEntity contactDetailsEntity, @PathVariable Long patientId) {
+        patientService.saveContactDetails(contactDetailsEntity, patientId);
         return new ResponseEntity<Object>(null, HttpStatus.OK);
     }
 }
